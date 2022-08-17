ebea15a58c2c7d7de840443e579263e44153438d
bichhuyen2109 <93117050+bichhuyen2109@users.noreply.github.com>
2022-08-17 22:36:46 +0700

    Update DSKQHT.cs

--- HeThongQuanLyTTHV/HeThongQuanLyTTHV/QLKQHT/DSKQHT.cs
+++ HeThongQuanLyTTHV/HeThongQuanLyTTHV/QLKQHT/DSKQHT.cs
@@ -31,10 +31,7 @@ namespace HeThongQuanLyTTHV.QLKQHT
             f.Chucnang = "chinh";
             f.Show();
         }
-<<<<<<< HEAD
 
-=======
->>>>>>> main
         private void btThoat_Click(object sender, EventArgs e)
         {
             this.Close();
@@ -43,11 +40,7 @@ namespace HeThongQuanLyTTHV.QLKQHT
         private void btXoa_Click(object sender, EventArgs e)
         {
             Them f = new Them();
-<<<<<<< HEAD
-            f.Chucnang = "xoa";
-=======
             f.Chucnang = "xoa"; 
->>>>>>> main
             f.Show();
         }
 
